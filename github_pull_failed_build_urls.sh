21d4b3581e45c7f059676a4a1fced89634fb12fb
Marcin Nowicki <pr0d1r2@gmail.com>
2017-03-09 13:37:10 +0100

    Unify scripts

--- github_pull_failed_build_urls.sh
+++ github_pull_failed_build_urls.sh
@@ -1,16 +1,13 @@
 function github_pull_failed_build_urls() {
-  local github_pull_failed_build_urls_CREDENTIALS=`github_api_credentials` || return $?
   local github_pull_failed_build_urls_PARAM
 
   for github_pull_failed_build_urls_PARAM in $@
   do
     github_is_pull_request $github_pull_failed_build_urls_PARAM
     if [ $? -eq 0 ]; then
-      local github_pull_failed_build_urls_STATUS_REF=`github_pull_last_hash $github_pull_failed_build_urls_PARAM`
-
       curl \
-        -u $github_pull_failed_build_urls_CREDENTIALS \
-        -i "$github_pull_failed_build_urls_REPO_URL/commits/$github_pull_failed_build_urls_STATUS_REF/status" 2>/dev/null | \
+        -u `github_api_credentials` \
+        -i "`github_api_url_from_pull_request $github_pull_failed_build_urls_PARAM`/commits/`github_pull_last_hash $github_pull_failed_build_urls_PARAM`/status" 2>/dev/null | \
           grep target_url | \
           cut -f 4 -d '"'
     fi
