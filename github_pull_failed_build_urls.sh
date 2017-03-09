63aa746e7953670d21837bdcce64cdb0b718a361
Marcin Nowicki <pr0d1r2@gmail.com>
2017-03-09 17:05:09 +0100

    Make scoping to failures work. With help from @h13ronim

--- github_pull_failed_build_urls.sh
+++ github_pull_failed_build_urls.sh
@@ -7,7 +7,8 @@ function github_pull_failed_build_urls() {
     if [ $? -eq 0 ]; then
       curl \
         -u `github_api_credentials` \
-        -i "`github_api_url_from_pull_request $github_pull_failed_build_urls_PARAM`/commits/`github_pull_last_hash $github_pull_failed_build_urls_PARAM`/status" 2>/dev/null | \
+        -i "`github_api_url_from_pull_request $github_pull_failed_build_urls_PARAM`/commits/`github_pull_last_hash $github_pull_failed_build_urls_PARAM`/statuses" 2>/dev/null | \
+          grep -A 2 '"state": "failure"' | \
           grep target_url | \
           cut -f 4 -d '"'
     fi
