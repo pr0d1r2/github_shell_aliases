793c344f71081c14e0d90428ed2ef60264e0a296
Marcin Nowicki <pr0d1r2@gmail.com>
2017-03-09 14:04:46 +0100

    Fix syntax

--- github_pull_last_hash.sh
+++ github_pull_last_hash.sh
@@ -2,8 +2,8 @@ function github_pull_last_hash() {
   github_is_pull_request $1 || return $?
   if [ $? -eq 0 ]; then
     local github_pull_last_hash_STATUSES_URL=`curl \
-      -u `github_api_credentials` \
-      -i "`github_api_url_from_pull_request $1`/pulls/`basename $1`" 2>/dev/null | \
+      -u \`github_api_credentials\` \
+      -i "\`github_api_url_from_pull_request $1\`/pulls/\`basename $1\`" 2>/dev/null | \
       grep statuses_url | \
       grep -v 'statuses/{sha}' | \
       cut -f 4 -d '"'`
