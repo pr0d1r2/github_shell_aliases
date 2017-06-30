44a7af67395aaff29a91b7d960d4a3ebfec82198
Marcin Nowicki <pr0d1r2@gmail.com>
2017-06-30 11:53:44 +0200

    Use GNU parallel for parallelisation

--- github_open.sh
+++ github_open.sh
@@ -1,9 +1,9 @@
 function github_open() {
   local github_open_ADDRESS=`git remote -v | grep fetch | sed -e 's|github.com:|github.com/|g' | cut -f 2 -d @ | cut -f 1-2 -d .`
-  local github_open_FILE
-  for github_open_FILE in $@
-  do
-    echorun open https://$github_open_ADDRESS/tree/master/$github_open_FILE &
-  done
-  wait
+  local github_open_COMMAND="open https://$github_open_ADDRESS/tree/master/{}"
+  parallel \
+    "echo '$github_open_COMMAND' && $github_open_COMMAND" \
+    ::: \
+    $@
+  return $?
 }
