fd7aea721680b9cccab49487879089ad1b660c67
Marcin Nowicki <pr0d1r2@gmail.com>
2017-07-19 07:46:50 +0200

    Allow to list specific users repos urls

--- github_repos_raw.sh
+++ github_repos_raw.sh
@@ -1,12 +1,20 @@
 function github_repos_raw() {
   local github_repos_raw_PAGE=1
   local github_repos_raw_RESULTS=100
+  case $1 in
+    "")
+      local github_repos_raw_URL="https://api.github.com/user/repos"
+      ;;
+    *)
+      local github_repos_raw_URL="https://api.github.com/users/$1/repos"
+      ;;
+  esac
   while [ $github_repos_raw_RESULTS -eq 100 ]
   do
     local github_repos_raw_RESULT=`
       curl \
         -u \`github_api_credentials\` \
-        -i "https://api.github.com/user/repos?per_page=100&page=$github_repos_raw_PAGE"
+        -i "$github_repos_raw_URL?per_page=100&page=$github_repos_raw_PAGE"
     `
     echo $github_repos_raw_RESULT
     github_repos_raw_RESULTS=`echo $(echo $github_repos_raw_RESULT | grep git_url | wc -l)`
