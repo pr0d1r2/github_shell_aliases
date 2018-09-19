185db7889e2001b9bff7e392dd25e1b475b14895
Marcin Nowicki <pr0d1r2@gmail.com>
2018-09-19 09:33:37 +0200

    Use better syntax

--- gpr.sh
+++ gpr.sh
@@ -5,6 +5,5 @@
 function gpr() {
   ensure_command git || return $?
   ensure_command hub || return $?
-  git push || return $?
-  hub pull-request || return $?
+  hub pull-request --push || return $?
 }
