d263801010858aa33b432ce65c7860e0597f4464
Marcin Nowicki <pr0d1r2@gmail.com>
2019-01-19 08:04:01 +0000

    Automatically apply labels on GitHub PRs

--- gpr.sh
+++ gpr.sh
@@ -5,14 +5,22 @@
 #
 # If you set up `github_user` function with your GitHub user inside
 # it  will be automatically set as an pull request Assignee.
+#
+# If you set up `github_pr_labels` function with your GitHub labels
+# mapped to your projects they will be automatically applied.
 function gpr() {
   local gpr_GITHUB_USER
+  local gpr_GITHUB_LABELS
+  local gpr_HUB_PARAMS
   ensure_command git || return $?
   ensure_command hub || return $?
   gpr_GITHUB_USER="$(github_user 2>/dev/null)"
+  gpr_GITHUB_LABELS="$(github_pr_labels 2>/dev/null)"
   if [ -n "$gpr_GITHUB_USER" ]; then
-    hub pull-request --push || return $?
-  else
-    hub pull-request --push -a "$gpr_GITHUB_USER" || return $?
+    gpr_HUB_PARAMS="-a '$gpr_GITHUB_USER'"
   fi
+  if [ -n "$gpr_GITHUB_LABELS" ]; then
+    gpr_HUB_PARAMS="-l '$gpr_GITHUB_LABELS'"
+  fi
+  hub pull-request --push $gpr_HUB_PARAMS || return $?
 }
