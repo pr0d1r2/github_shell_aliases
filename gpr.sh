3ee361cea6550d75d99a0218212ca15758d130c3
Marcin Nowicki <pr0d1r2@gmail.com>
2019-01-19 07:41:36 +0000

    Automatically set Asignee on pull request

--- gpr.sh
+++ gpr.sh
@@ -2,8 +2,17 @@
 #
 # Example usage:
 #   gpr
+#
+# If you set up `github_user` function with your GitHub user inside
+# it  will be automatically set as an pull request Assignee.
 function gpr() {
+  local gpr_GITHUB_USER
   ensure_command git || return $?
   ensure_command hub || return $?
-  hub pull-request --push || return $?
+  gpr_GITHUB_USER="$(github_user 2>/dev/null)"
+  if [ -n "$gpr_GITHUB_USER" ]; then
+    hub pull-request --push || return $?
+  else
+    hub pull-request --push -a "$gpr_GITHUB_USER" || return $?
+  fi
 }
