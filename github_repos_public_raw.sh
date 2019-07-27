85f1d7adbc9c6dd700811634d0d651e833716cff
Marcin Nowicki <pr0d1r2@gmail.com>
2019-07-27 11:23:42 +0200

    Get public repos urls of specific user/organization

--- github_repos_public_raw.sh
+++ github_repos_public_raw.sh
@@ -1,15 +1,27 @@
 function github_repos_public_raw() {
-  local github_repos_public_raw_PAGE=1
-  local github_repos_public_raw_RESULTS=100
+  local github_repos_public_raw_GITHUB_USER
+  local github_repos_public_raw_PAGE
+  local github_repos_public_raw_RESULTS
+  local github_repos_public_raw_RESULT
+  github_repos_public_raw_PAGE=1
+  github_repos_public_raw_RESULTS=100
+  case $1 in
+    "")
+      github_repos_public_raw_GITHUB_USER="$(github_username)"
+      ;;
+    *)
+      github_repos_public_raw_GITHUB_USER="$1"
+      ;;
+  esac
   while [ $github_repos_public_raw_RESULTS -eq 100 ]
   do
-    local github_repos_public_raw_RESULT=`
+    github_repos_public_raw_RESULT=$(
       curl \
-        -u \`github_api_credentials\` \
-        -i "https://api.github.com/users/\`github_username\`/repos?per_page=100&page=$github_repos_public_raw_PAGE"
-    `
-    echo $github_repos_public_raw_RESULT
-    github_repos_public_raw_RESULTS=`echo $(echo $github_repos_public_raw_RESULT | grep git_url | wc -l)`
-    github_repos_public_raw_PAGE=`expr $github_repos_public_raw_PAGE + 1`
+      -u "$(github_api_credentials)" \
+      -i "https://api.github.com/users/$github_repos_public_raw_GITHUB_USER/repos?per_page=100&page=$github_repos_public_raw_PAGE"
+    )
+    echo "$github_repos_public_raw_RESULT"
+    github_repos_public_raw_RESULTS=$(echo $(echo "$github_repos_public_raw_RESULT" | grep -c git_url))
+    github_repos_public_raw_PAGE=$((github_repos_public_raw_PAGE + 1))
   done
 }
--- github_repos_public_urls.sh
+++ github_repos_public_urls.sh
@@ -1,3 +1,7 @@
+# Returns public repos URLs for GitHub users or organizations
+#
+# Example usage:
+#   github_repos_public_urls pr0d1r2
 function github_repos_public_urls() {
-  github_repos_public_raw 2>/dev/null | grep git_url | cut -f 4 -d '"'
+  github_repos_public_raw $* 2>/dev/null | grep git_url | cut -f 4 -d '"'
 }
